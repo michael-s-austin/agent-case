4cb42aae6a84a81a75147ae4b2d9c676300fccbc
Michael Austin <michaelaustin78@gmail.com>
2026-05-28 21:20:16 -0500

    Added: print prompt and response token usage metadata from Gemini API response

--- main.py
+++ main.py
@@ -11,4 +11,10 @@ if api_key is None:
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-print (response.text)
+
+if response.usage_metadata is None:
+    raise RuntimeError("There is no usage metadata available.")
+
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print(response.text)

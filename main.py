c32eece3a096330eb4eb6cdf8770f8de3df62180
Michael Austin <michaelaustin78@gmail.com>
2026-05-30 10:26:53 -0500

    Added: verbose flag to display prompt and token usage statistics

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import argparse
 
@@ -8,6 +9,7 @@ from google.genai import types
 def main() -> None:
     parser = argparse.ArgumentParser(description="Case")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     load_dotenv()
@@ -19,9 +21,9 @@ def main() -> None:
     messages: list[types.Content] = [
         types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
     ]
-    generate_content(client, messages)
+    generate_content(client, messages, args.verbose, args.user_prompt)
 
-def generate_content(client: genai.Client, messages: list[types.Content]) -> None:
+def generate_content(client: genai.Client, messages: list[types.Content], verbose: bool, user_prompt: str) -> None:
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
         contents=messages,
@@ -29,10 +31,13 @@ def generate_content(client: genai.Client, messages: list[types.Content]) -> Non
     if not response.usage_metadata:
         raise RuntimeError("There is no usage metadata available.")
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
     print("Response:")  
     print(response.text)
-
+        
 if __name__=="__main__":
     main() 

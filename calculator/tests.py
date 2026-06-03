993d1c1ffd23b421b8f9bf944f609ee3fdae765e
Michael Austin <michaelaustin78@gmail.com>
2026-06-03 10:51:57 -0500

    feat: add get_file_content utility and test suite with sample text file

--- calculator/tests.py
+++ calculator/tests.py
@@ -1,7 +1,7 @@
 # calculator/tests.py
 
 import unittest
-from pkg.calculator import Calculator
+from .pkg.calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
     def setUp(self) -> None:

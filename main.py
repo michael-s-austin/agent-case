f2a0a65cad3e632877374c553ebd86185555e9fe
Michael Austin <michaelaustin78@gmail.com>
2026-06-10 10:56:52 -0500

    feat: implement dynamic function dispatching with working directory support and update main loop to handle function call execution

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Case")
@@ -42,8 +42,31 @@ def generate_content(client: genai.Client, messages: list[types.Content], verbos
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     
     if response.function_calls:
+        
+        function_results = []
+
         for function_call in response.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
+            
+            try:
+
+                function_call_result = call_function(function_call, verbose)
+                
+                if not function_call_result.parts:
+                    raise Exception(f"Error: No parts found")
+                
+                if not function_call_result.parts[0].function_response:
+                    raise Exception(f"Error: No parts found")
+                
+                if not function_call_result.parts[0].function_response.response:
+                    raise Exception(f"Error: No parts found")
+                
+                function_results.append(function_call_result.parts[0])
+
+                if verbose:
+                    print(f" -> {function_call_result.parts[0].function_response.response}")
+
+            except Exception as e:
+                return f"Error: {e}"
     else:
         print("Response:")  
         print(response.text)

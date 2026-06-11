4b7b6552579dad4d0c513219dff89086ebca6859
Michael Austin <michaelaustin78@gmail.com>
2026-06-11 10:26:57 -0500

    refactor: move generate_content logic to a dedicated module and remove redundant argparse import

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import argparse
 import os
-import argparse
 
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
 from call_function import available_functions, call_function
+from generate_content import generate_content
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Case")
@@ -24,52 +24,6 @@ def main() -> None:
         types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
     ]
     generate_content(client, messages, args.verbose, args.user_prompt)
-
-def generate_content(client: genai.Client, messages: list[types.Content], verbose: bool, user_prompt: str) -> None:
-    response = client.models.generate_content(
-        model='gemini-2.5-flash', 
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt),
-    )
-    if not response.usage_metadata:
-        raise RuntimeError("There is no usage metadata available.")
-
-    if verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    
-    if response.function_calls:
-        
-        function_results = []
-
-        for function_call in response.function_calls:
-            
-            try:
-
-                function_call_result = call_function(function_call, verbose)
-                
-                if not function_call_result.parts:
-                    raise Exception(f"Error: No parts found")
-                
-                if not function_call_result.parts[0].function_response:
-                    raise Exception(f"Error: No parts found")
-                
-                if not function_call_result.parts[0].function_response.response:
-                    raise Exception(f"Error: No parts found")
-                
-                function_results.append(function_call_result.parts[0])
-
-                if verbose:
-                    print(f" -> {function_call_result.parts[0].function_response.response}")
-
-            except Exception as e:
-                return f"Error: {e}"
-    else:
-        print("Response:")  
-        print(response.text)
         
 if __name__=="__main__":
     main() 
